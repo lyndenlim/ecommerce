a803c35cc55810203334cf7fde7df8467f7a249b
lynden <lyndenlim15@gmail.com>
2022-05-17 14:25:45 -0400

    Get fetch to render

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
 import './App.css';
+import axios, { AxiosResponse } from "axios"
+import { useEffect, useState } from 'react';
 
-function App() {
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+}
+
+const App: React.FC = () => {
+  const [products, setProducts] = useState<Product[]>([])
+
+  useEffect(() => {
+    async function getProductData() {
+      axios.get("https://fakestoreapi.com/products")
+        .then((response: AxiosResponse) => {
+          setProducts(response.data)
+        })
+
+    }
+
+    getProductData()
+  }, [])
+
   return (
     <div className="App">
+      {products.map(product => <div key={product.id}>{product.title}</div>)}
     </div>
   );
 }
