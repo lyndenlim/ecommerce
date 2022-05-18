be871203fbfb1eec40d1996f0ce592f7063ac1ba
lynden <lyndenlim15@gmail.com>
2022-05-18 13:52:31 -0400

    Add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import ProductPage from "./Components/ProductPage/ProductPage"
 import IndividualProduct from "./Components/IndividualProduct/IndividualProduct"
+import Navbar from "./Components/Navbar/Navbar"
 import { Routes, Route } from "react-router-dom"
 
 const App: React.FC = () => {
   return (
     <div className="item-container">
+      <Navbar />
       <Routes>
         <Route path="/" element={<ProductPage />} />
         <Route path="/products/:id" element={<IndividualProduct />} />
---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+const Navbar: React.FC = () => {
+    return (
+        <nav>
+            <Link to="/">Home</Link>
+        </nav>
+    )
+}
+
+export default Navbar
